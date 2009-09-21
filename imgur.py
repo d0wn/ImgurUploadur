803e6cb01dffae00621b89e67d0e253fea9bc755
d0wn <lold0wn@gmail.com>
2009-09-21 07:52:48 -0400

    Successful Image upload, and XML stored into string

--- imgur.py
+++ imgur.py
@@ -3,6 +3,7 @@
 import pycurl
 import re
 import os.path
+from cStringIO import StringIO
 
 def pUpload(pLoc):
     # Function to parse the API's XML and figure out if the image is a URL or local file 
@@ -17,14 +18,12 @@ def cUpload(imgLoc, isURL):
     
     if isURL == 1:
         # if os.path.isfile("/tmp/imgurTmpImg") == true: os.unlink("/tmp/imgurTmpImg")
-        from cStringIO import StringIO
         tmpFile = "/tmp/imgurTmpImg"
         tmpFileLoc = open(tmpFile,  "w")
         cGet  = pycurl.Curl()
-        #buffer = StringIO()
         cGet.setopt(cGet.URL,  imgLoc)
         cGet.setopt(cGet.WRITEDATA,  tmpFileLoc)
-        cGet.setopt(cGet.VERBOSE,  0)
+        cGet.setopt(cGet.VERBOSE,  1)
         #cGet.perform()
       #  output = open(tmpFileLoc,  "w")
       # output.write(buffer.getvalue)
@@ -37,13 +36,14 @@ def cUpload(imgLoc, isURL):
     
     if isURL == 0: values = [ ("key", "c4eb08d39a32e5a71c3df7225f137f06221476db"),  ("image", (cPost.FORM_FILE,  imgLoc)) ]
     cPost.setopt(cPost.POST, 1)
-    cPost.setopt(cPost.VERBOSE,  1)
+    cPost.setopt(cPost.VERBOSE,  0)
     cPost.setopt(cPost.URL, "http://imgur.com/api/upload/")
     cPost.setopt(cPost.HTTPPOST, values)
-    postOut = ""
-    cPost.setopt(cPost.WRITEDATA, postOut) 
+    buffer = StringIO()
+    cPost.setopt(cPost.WRITEFUNCTION,  buffer.write)
     # if os.path.isfile("/tmp/imgurTmpImg") == true: os.unlink("/tmp/imgurTmpImg")
-    return postOut
+    cPost.perform()
+    return buffer.getvalue()
     
 
-cUpload("http://208.90.224.244/~corncunt/logo.gif",  1)
+cUpload("./smiley2.png",  0)
