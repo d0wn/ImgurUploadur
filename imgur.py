46681ec040a21ba8f035a0c302043014372467ba
d0wn <imgur.uploadur@gmail.com>
2009-10-04 21:37:37 -0400

    Modified: imgur.py

--- imgur.py
+++ imgur.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
+# Copyright 2009 d0wn
 
 #just a little info before any of you try using my code, i have no idea what i'm doing, and there WILL be bugs
+
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import pycurl
 import re
